925854a19b0bf3c9c600f39fe6509bb586b6f55b
Stanley Louis <stanlouis@msn.com>
2019-04-14 17:13:45 -0400

    Set up mongoose connection

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 const app = express();
 
 const indexRouter = require('./routes/index');
@@ -17,8 +22,13 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
+
 app.use('/', indexRouter);
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => `Server is listening on port ${port}`);
+app.listen(PORT, () => `Server is listening on port ${PORT}`);
